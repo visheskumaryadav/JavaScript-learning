77911da981b86462222c9a6c5ea611afb0b18bf0
vishes kumar yadav <vishes7576@gmail.com>
2023-12-20 23:43:09 +0530

    Fundaments: Variable are done

---
+++ JS/1_Fundamentals/variables.js
@@ -0,0 +1,50 @@
+// const roll_no=25
+// let name="vishesh"
+// var age=15
+// bloodGroup="b+"
+
+// // roll_no=90   can't reassign to const
+// console.log(typeof bloodGroup);
+
+// console.log(cat);
+// var cat="animal"
+
+// console.log(kid)
+// let kid=55;  
+// let home
+// console.log(home);
+
+// console.log(beta);
+// const beta;
+
+// var first=11
+// let second=22
+// function fun(){
+//     console.log(first);
+//     console.log(second);
+//     //let third=33
+//     var fourth=44
+// }
+// // console.log(third);
+// console.log(fourth);
+// fun()
+
+// if(true){
+//     var num1=90
+//     let num2=20
+// }
+// console.log(num1);
+// console.log(num2);
+
+/*
+const: a const variable means once it is defined then its value 
+       cannot be changed. we cannot reassign the value
+       we need to declared it before using it.
+let: a let variable means once it is defined we can reassign another value
+     we need to declared it before using it
+     it has block scope.
+     if we use it without defining it then we get "undefined"
+var: a var variable has function scope not block  scope
+     And we can use it before declaration
+     
+*/
